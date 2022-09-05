06417407cf5f4b9bc6af16b296098875552a0d6b
Priyen Pindoria <priyendevraj.pindoria@students.mq.edu.au>
2022-09-06 08:41:53 +1000

    Initial commit

---
+++ R/praise.R
@@ -0,0 +1,16 @@
+#' Deliver Praise
+#'
+#' @description This function is super useful when wanting praise
+#'
+#' @param name text string; Name of person to be praised
+#' @param punctuation text string; This is our emphasis
+#'
+#' @return Text string with praise
+#' @export
+#'
+#' @examples
+#' praise(name = "Steph", punctuation = "!")
+praise <- function(name, punctuation = "!") {
+  glue::glue("You're the best, {name}{punctuation}")
+
+}
